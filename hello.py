55eef5f2007af3b78bb303bd2edc3183e8417472
Esteban <mail@estebancervantes.com>
2021-03-18 15:21:03 -0600

    added error catching

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from cloudant import Cloudant
 from flask import Flask, render_template, request, jsonify
 import atexit
+from flask_cors import CORS, cross_origin
 import os
 import json
 import json
@@ -15,22 +16,30 @@ tone_analyzer = ToneAnalyzerV3(
 tone_analyzer.set_service_url(os.environ.get('API_URL',""))
 
 app = Flask(__name__, static_url_path='')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 port = int(os.getenv('PORT', 8000))
 
+@app.route('/', methods=['GET'])
+@cross_origin()
+def rootGet():
+    return f"Welcome to Tone API by ESTEBAN DWORAK. Send a JSON POST request with a 'text' property"
+
+
 @app.route('/', methods=['POST'])
+@cross_origin()
 def root():
-    content = request.json
-    
-    text = content["text"]
-
-    tone_analysis = tone_analyzer.tone(
-        {'text': text},
-        content_type='application/json'
-    ).get_result()
-
-
-    return tone_analysis
+    try:
+        content = request.json
+        text = content["text"]
+        tone_analysis = tone_analyzer.tone(
+            {'text': text},
+            content_type='application/json'
+        ).get_result()
+        return tone_analysis
+    except:
+        return {"success":false,"msg": "An error ocurred while fetching Tone analyzer API."}
 
 
 if __name__ == '__main__':
